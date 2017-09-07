1ee738eba22c71f50d9ec3ba1258f92673e8da70
Daniel <danielrobinson095@gmail.com>
2017-09-07 00:17:25 -0400

    Now able to loop through board and detect winners

--- js/connectfour.js
+++ js/connectfour.js
@@ -64,10 +64,84 @@ function checkForWinner(){
  var verticalWinner = 0;
  var diagnolWinner = 0;
 
-  for(i = 5; i >= 0; i++){
-    for(j = 0; j < 7; j++){
+  //r5c6
+
+  //Check for horizontal winner
+  //Loop through rows
+  for (i = 0; i < 6; i++) {
+    //Loop through columns
+    for (j = 0; j < 7; j++) {
+      if(document.getElementById('r' + i + 'c' + j).style.backgroundColor == document.currentPlayer){
+        horizontalWinner++;
+        if(horizontalWinner == 4){
+          console.log('we have a horiz winner');
+        }
+      } else {
+        horizontalWinner = 0;
+      }
 
     }
   }
+  //Check for vertical winner
+  //loop trough columns
+  for (i = 0; i < 7; i++) {
+    //Loop through rows
+    for (j = 0; j < 6; j++){
+      if(document.getElementById('r' + j + 'c' + i).style.backgroundColor == document.currentPlayer){
+        verticalWinner++;
+        if(verticalWinner == 4){
+          console.log('we have a vert winner');
+        }
+      } else{
+        verticalWinner = 0;
+      }
+    }
+  }
+
+  // Check for diagnol winner.
+  //Loop through rows
+
+  for(var j = 0; j < 3; j++){
+    for (var i = 0; i < 6; i++) {
+      if(document.getElementById('r' + (i+j) + 'c' + i) != null){
+
+      }
+    }
+  }
+
+
+  for(var j = 0; j < 4; j++){
+    for (var i = 0; i < 6; i++) {
+      if(document.getElementById('r' + i + 'c' + (i+j)) != null){
+
+      }
+    }
+  }
+
+for(var row = 0; row < 3; row++){
+  var tempRow = row;
+  for(var column = 6; column >= 0; column--){
+    //console.log(document.getElementById('r' + row++ + 'c' + column))
+  }
+  row = tempRow;
+}
+
+
+var counter = 0;
+
+for(var row = 0; row < 3; row++){
+  row = 0;
+  for(var column = 5; column >= 0; column --){
+
+    if((column - counter) >= 0){
+      console.log(document.getElementById('r' + row++ + 'c' + (column - counter)))
+    }
+  }
+  row = 0;
+  counter++;
+  if(counter == 3){
+    break;
+  }
+}
 
 }
